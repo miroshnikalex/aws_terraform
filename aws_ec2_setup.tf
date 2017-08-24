5cce6d6e2f33084cfe55a789d04e795f622bd60a
IJ2RW4Y8 <oleksandr.miroshnyk@accenture.com>
2017-08-24 17:55:56 +0200

    new version with vars

--- aws_ec2_setup.tf
+++ aws_ec2_setup.tf
@@ -25,9 +25,9 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "server-ansible" {
-  ami           = "ami-657bd20a"
-  instance_type = "t2.micro"
-  key_name      = "Free-tier"
+  ami           = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
   availability_zone= "eu-central-1a"
   associate_public_ip_address = "true"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
@@ -39,87 +39,20 @@ resource "aws_instance" "server-ansible" {
       "sudo yum-config-manager --enable epel",
       "sudo yum clean all",
       "sudo yum install ansible -y",
-      "sudo yum update -y"
-    ]
+      "sudo yum update -y",
+      "sudo useradd ${var.ansible_user} -p ${var.ansible_password}",
+      "sudo sed -i 's/PermitRootLogin forced-commands-only/#PermitRootLogin forced-commands-only/g' /etc/ssh/sshd_config",
+      "sudo sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config",
+      "sudo su -",
+      "touch /etc/sudoers.d/${var.ansible_user}",
+      "echo '${var.ansible_user} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/${var.ansible_user}",
+      "service sshd reload",
+      "exit"
+        ]
     connection {
       type = "ssh"
       user = "ec2-user"
       private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
     }
   }
-}
-
-resource "aws_instance" "node-ansible-1" {
-  ami           = "ami-657bd20a"
-  instance_type = "t2.micro"
-  key_name      = "Free-tier"
-  availability_zone= "eu-central-1a"
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  tags {
-       Name = "node-ansible-1"
-       }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum-config-manager --enable epel",
-      "sudo yum clean all",
-      "sudo yum install ansible -y",
-      "sudo yum update -y"
-         ]
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
-    }
-  }
-}
-
-resource "aws_instance" "node-ansible-2" {
-  ami           = "ami-657bd20a"
-  instance_type = "t2.micro"
-  key_name      = "Free-tier"
-  availability_zone= "eu-central-1b"
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  tags {
-       Name = "node-ansible-2"
-       }
-       provisioner "remote-exec" {
-         inline = [
-           "sudo yum-config-manager --enable epel",
-           "sudo yum clean all",
-           "sudo yum install ansible -y",
-           "sudo yum update -y"
-         ]
-         connection {
-           type = "ssh"
-           user = "ec2-user"
-           private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
-         }
-       }
-}
-
-resource "aws_instance" "node-ansible-3" {
-  ami           = "ami-657bd20a"
-  instance_type = "t2.micro"
-  key_name      = "Free-tier"
-  availability_zone= "eu-central-1c"
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  tags {
-       Name = "node-ansible-3"
-       }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum-config-manager --enable epel",
-      "sudo yum clean all",
-      "sudo yum install ansible -y",
-      "sudo yum update -y"
-         ]
-    connection {
-    type = "ssh"
-    user = "ec2-user"
-    private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
-  }
- }
 }
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "ansible_user" {
+  default = "ansible-user"
+}
+variable "ansible_password" {
+  default = "SX2gJPqgsyK4CfHp"
+}
+variable "region" {
+  default = "eu-central-1"
+}
+variable "ami" {
+  default = "ami-657bd20a"
+}
+variable "key_name" {
+  default = "Free-tier"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
