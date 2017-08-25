42ba164d4fc6648d31417e2fab41652d85cd3202
IJ2RW4Y8 <oleksandr.miroshnyk@accenture.com>
2017-08-25 15:06:56 +0200

    new version

--- aws_ec2_setup.tf
+++ aws_ec2_setup.tf
@@ -1,58 +1,32 @@
 provider "aws" {
-  region     = "eu-central-1"
-  profile    = "Alex"
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name		= "allow_ssh"
-  description	= "Allow SSH access to the resources"
-  tags {
-       Name = "allow_ssh"
-       }
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks  = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  region     = "${var.AWS_REGION}"
+  profile    = "${var.AWS_PROFILE_NAME}"
 }
 
 resource "aws_instance" "server-ansible" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${var.key_name}"
-  availability_zone= "eu-central-1a"
+  ami           = "${lookup(var.AWS_AMI, var.AWS_REGION)}"
+  instance_type = "${var.AWS_INSTANCE_TYPE}"
+  key_name      = "${var.AWS_KEY_NAME}"
   associate_public_ip_address = "true"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   tags {
        Name = "server-ansible"
        }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum-config-manager --enable epel",
-      "sudo yum clean all",
-      "sudo yum install ansible -y",
-      "sudo yum update -y",
-      "sudo useradd ${var.ansible_user} -p ${var.ansible_password}",
-      "sudo sed -i 's/PermitRootLogin forced-commands-only/#PermitRootLogin forced-commands-only/g' /etc/ssh/sshd_config",
-      "sudo sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config",
-      "sudo su -",
-      "touch /etc/sudoers.d/${var.ansible_user}",
-      "echo '${var.ansible_user} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers.d/${var.ansible_user}",
-      "service sshd reload",
-      "exit"
+provisioner "remote-exec" {
+  inline = [
+    "sudo rpm -Uhv https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+    "sudo yum clean all",
+    "sudo yum install ansible -y",
+    "sudo yum update -y",
+    "sudo useradd ${var.ANSIBLE_USER} -p ${var.ANSIBLE_PASSWORD}",
+    "sudo sed -i 's/PermitRootLogin forced-commands-only/#PermitRootLogin forced-commands-only/g' /etc/ssh/sshd_config",
+    "sudo sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config",
+    "sudo systemctl reload sshd"
         ]
-    connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = "${file("~/terraform/.ssh/aws_ec2_Alex")}"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,20 @@
-variable "ansible_user" {
-  default = "ansible-user"
-}
-variable "ansible_password" {
-  default = "SX2gJPqgsyK4CfHp"
-}
-variable "region" {
+variable "ANSIBLE_USER" {}
+variable "ANSIBLE_PASSWORD" {}
+variable "AWS_REGION" {
   default = "eu-central-1"
 }
-variable "ami" {
-  default = "ami-657bd20a"
+variable "AWS_AMI" {
+  type = "map"
+  default = {
+    eu-central-1 = "ami-d74be5b8"
+    eu-west-1    = "ami-ebd02392"
+    eu-west-2    = "ami-a1f5e4c5"
+    ca-central-1 = "ami-9062d0f4"
+    us-east-1    = "ami-c998b6b2"
+  }
 }
-variable "key_name" {
-  default = "Free-tier"
-}
-variable "instance_type" {
+variable "AWS_KEY_NAME" {}
+variable "AWS_INSTANCE_TYPE" {
   default = "t2.micro"
 }
+variable "AWS_PROFILE_NAME" {}
